3adccc71699f9ec98314081a57cdbbee715b072e
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-10-31 04:43:46 +0530

    SHOPPING PRODUCT LIST UI(Products.jsx component and Product component)

--- src/data.js
+++ src/data.js
@@ -42,3 +42,44 @@ export const categories = [
         title: "LIGHT JACKETS!",
     },
 ];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://cdn.shopify.com/s/files/1/0752/6435/products/florengreeeg.jpg?v=1622786087",
+        
+    },
+    {
+        id: 2,
+        img: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimagesvc.meredithcorp.io%2Fv3%2Fmm%2Fimage%3Furl%3Dhttps%253A%252F%252Fstatic.onecms.io%252Fwp-content%252Fuploads%252Fsites%252F14%252F2020%252F03%252F17%252F031720_celeb_loungewear_alessandra_embed_2-2000.jpg&q=85",
+        
+    },
+    {
+        id: 3,
+        img: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7371458/2018/11/12/c2515544-f7ee-4538-9f72-fd79dcbe15961542003256477-HERENOW-Women-Jackets-6011542003255450-1.jpg",
+    },
+
+    {
+        id: 4,
+        img: "https://cdn.shopify.com/s/files/1/0752/6435/products/florengreeeg.jpg?v=1622786087",
+        
+    },
+    {
+        id: 5,
+        img: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimagesvc.meredithcorp.io%2Fv3%2Fmm%2Fimage%3Furl%3Dhttps%253A%252F%252Fstatic.onecms.io%252Fwp-content%252Fuploads%252Fsites%252F14%252F2020%252F03%252F17%252F031720_celeb_loungewear_alessandra_embed_2-2000.jpg&q=85",
+        
+    },
+    {
+        id: 6,
+        img: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7371458/2018/11/12/c2515544-f7ee-4538-9f72-fd79dcbe15961542003256477-HERENOW-Women-Jackets-6011542003255450-1.jpg",
+    },
+    {
+        id: 7,
+        img: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimagesvc.meredithcorp.io%2Fv3%2Fmm%2Fimage%3Furl%3Dhttps%253A%252F%252Fstatic.onecms.io%252Fwp-content%252Fuploads%252Fsites%252F14%252F2020%252F03%252F17%252F031720_celeb_loungewear_alessandra_embed_2-2000.jpg&q=85",
+        
+    },
+    {
+        id: 8,
+        img: "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7371458/2018/11/12/c2515544-f7ee-4538-9f72-fd79dcbe15961542003256477-HERENOW-Women-Jackets-6011542003255450-1.jpg",
+    },
+];
