3ae6e3afd08c226194431d163cd9e83d1ed7e155
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-10-30 16:17:43 +0530

    Creating_pure_react_slider

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const sliderItems = [
+    {
+     id: 1,
+     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtRrHXPKnD8z_iuf_jV3sBklL2zR8E9qQGCw&usqp=CAU",
+     title: "SUMMER SALE",
+     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia itaque adipisci ullam nobis reprehenderit quo veniam minus animi quasi fuga.",
+     bg: "f5ddfd",
+    },
+
+    {
+        id: 2,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtRrHXPKnD8z_iuf_jV3sBklL2zR8E9qQGCw&usqp=CAU",
+        title: "WINTER SALE",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia itaque adipisci ullam nobis reprehenderit quo veniam minus animi quasi fuga.",
+        bg: "f5fafd",
+       },
+
+       {
+        id: 3,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtRrHXPKnD8z_iuf_jV3sBklL2zR8E9qQGCw&usqp=CAU",
+        title: "POPULAR SALE",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia itaque adipisci ullam nobis reprehenderit quo veniam minus animi quasi fuga.",
+        bg: "f5fafd",
+       },
+
+];
