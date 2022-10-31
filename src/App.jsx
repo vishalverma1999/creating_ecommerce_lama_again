38ba43b795ff79cab112a23d60a111cf46c3e5a7
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-10-31 17:22:37 +0530

    SHOPPING CATEGORY PAGE(ProductList.jsx page component)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Home />;
+  return (
+    // <Home />
+    <ProductList />
+  );
 };
 
 export default App;
---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,81 @@
+import styled from "styled-components"
+import Navbar from "../components/Navbar"
+import Announcement from "../components/Announcement"
+import Products from "../components/Products"
+import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
+
+
+const Container = styled.div``
+const Title = styled.h1`margin: 20px;`
+
+const FilterContainer = styled.div`
+display: flex;
+justify-content: space-between;
+`
+const Filter = styled.div`
+/* display: flex;
+align-items: center; */
+margin: 20px;
+`
+const FilterText = styled.span`
+font-size: 20px;
+font-weight: 600;
+margin-right: 20px;
+`
+
+
+const Select = styled.select`
+ /* border: 2px solid red; */
+ padding: 10px;
+ margin-right: 20px;
+`
+const Option = styled.option`
+
+`
+
+const ProductList = () => {
+    return (
+        <Container>
+            <Navbar />
+            <Announcement />
+            <Title>Dresses</Title>
+            <FilterContainer>
+                <Filter>
+                    <FilterText>Filter Products:</FilterText>
+                    <Select defaultValue="Color">
+                        <Option disabled >Color</Option>     {/*disabled selected means by default to color selected rahega par disabled mode mein hai means select or unselect from drop down list */}
+                        <Option >White</Option>
+                        <Option>Black</Option>
+                        <Option>Red</Option>
+                        <Option>Blue</Option>
+                        <Option>Yellow</Option>
+                        <Option>Green</Option>
+                    </Select>
+
+                    <Select defaultValue="Size">
+                        <Option disabled  >Size</Option>
+                        <Option>XS</Option>
+                        <Option>S</Option>
+                        <Option>M</Option>
+                        <Option>L</Option>
+                        <Option>XL</Option>
+                    </Select>
+                </Filter>
+                <Filter>
+                <FilterText>Sort Products:</FilterText>
+                <Select defaultValue="Newest">
+                        <Option>Newest</Option>   {/*selected means by default to color selected rahega means we can select or unselect from drop down list */}
+                        <Option>Price (asc)</Option>
+                        <Option>Price (desc)</Option>
+                    </Select>
+                </Filter>
+            </FilterContainer>
+            <Products/>
+            <Newsletter/>
+            <Footer/>
+        </Container>
+    )
+}
+
+export default ProductList
