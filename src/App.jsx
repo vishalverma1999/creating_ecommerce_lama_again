037fac7fc01ba2b783ed539a7183872c898dab2f
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-10-31 21:17:28 +0530

    REACT LOGIN AND REGISTER PAGE DESIGN(Login.jsx and Register.jsx page component)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     // <Home />
     // <ProductList />
-    <Product />
+    // <Product />
+    <Register />
+    // <Login />
   );
 };
 
---
+++ src/pages/Register.jsx
@@ -0,0 +1,73 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+// since it is going to be full screen component therefore 100vw and 100vh
+width: 100vw; 
+height: 100vh;
+// uSING linear gradient b/c image is very sharp
+background: linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)) ,url("https://images.pexels.com/photos/1536619/pexels-photo-1536619.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") center center no-repeat;
+/* opacity: 0.5; */
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
+const Wrapper = styled.div`
+width: 40%;
+padding: 20px;
+background-color: white;
+`
+
+const Title = styled.h1`
+font-size: 24px;
+font-weight: 300;
+`
+const Form = styled.form`
+display: flex;
+flex-wrap: wrap;
+`
+
+const Input = styled.input`
+flex: 1;
+min-width: 40%;        // alternatively we can write-->  flex: 1 0 40%;  this will do the work of two lines in one line
+margin: 20px 10px 0px 0px;
+padding: 10px;
+`
+
+const Agreement = styled.span`
+font-size: 12px;
+margin: 20px 0px;
+`
+
+const Button = styled.button`
+width: 40%;
+border: none;
+padding: 15px 20px;
+cursor: pointer;
+color: white;
+background-color: teal;
+`
+
+const Register = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>CREATE AN ACCOUNT</Title>
+                <Form>
+                    <Input placeholder="name" />
+                    <Input placeholder="last name" />
+                    <Input placeholder="username" />
+                    <Input placeholder="email" />
+                    <Input placeholder="password" />
+                    <Input placeholder="confirm password" />
+                    <Agreement>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At delectus enim distinctio doloremque, sequi consequuntur! <b>PRIVACY POLICY</b>
+                    </Agreement>
+                    <Button>CREATE</Button>
+                </Form>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Register
