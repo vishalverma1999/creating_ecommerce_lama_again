fff194ca34c278e3a0ee12d6bd74683b0ecc2a7d
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-11-02 19:00:59 +0530

    Node.js mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');  
 const app = express(); 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-app.listen(5000, ()=>{              // if process.env.PORT is available use it else use 5000
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=> console.log("DB connection successful"))
+.catch((err)=> console.log(err));
+
+app.listen(process.env.PORT || 5000, ()=>{              // if process.env.PORT is available use it else use 5000
     console.log("backend server is running")
 });
