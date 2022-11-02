d056b7e606e59bd9d70778ad2fb83aa476673baa
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-11-02 19:26:16 +0530

    Understanding Node.js router

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express(); 
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoute = require('./routes/user')
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=> console.log("DB connection successful"))
 .catch((err)=> console.log(err));
+
+app.use(express.json());  
+app.use('/api/users', userRoute);     // jab aap post req bhejoge json mein to ye nahi chalega, b/c our application is not able to any json object. to prevent this write --> app.use(express.json()) before routes;
 
 app.listen(process.env.PORT || 5000, ()=>{              // if process.env.PORT is available use it else use 5000
     console.log("backend server is running")
