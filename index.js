ccce4eee58156e8530b844ebe686991143030964
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-11-03 00:49:42 +0530

    Node.js mongodb authentication

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json());  
 app.use('/api/users', userRoute);     // jab aap post req bhejoge json mein to ye nahi chalega, b/c our application is not able to any json object. to prevent this write --> app.use(express.json()) before routes;
+app.use('/api/auth', authRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{              // if process.env.PORT is available use it else use 5000
     console.log("backend server is running")
