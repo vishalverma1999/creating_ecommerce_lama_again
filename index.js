61bf3dfc5a49719a1477ff2a663ad337082b0674
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-11-04 00:18:30 +0530

    product_route

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express = require('express');  
-const app = express(); 
+const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth')
+const productRoute = require('./routes/product')
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
-.then(()=> console.log("DB connection successful"))
-.catch((err)=> console.log(err));
+    .then(() => console.log("DB connection successful"))
+    .catch((err) => console.log(err));
 
-app.use(express.json());  
+app.use(express.json());
 app.use('/api/users', userRoute);     // jab aap post req bhejoge json mein to ye nahi chalega, b/c our application is not able to any json object. to prevent this write --> app.use(express.json()) before routes;
 app.use('/api/auth', authRoute);
+app.use('/api/products', productRoute);
 
-app.listen(process.env.PORT || 5000, ()=>{              // if process.env.PORT is available use it else use 5000
+app.listen(process.env.PORT || 5000, () => {              // if process.env.PORT is available use it else use 5000
     console.log("backend server is running")
 });
