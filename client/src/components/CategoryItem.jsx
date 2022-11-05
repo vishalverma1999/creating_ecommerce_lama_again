23a9b98ceeb19f933ecbb20de9440d27f47b88e3
vishalverma1999 <vermavishal1999.vv@gmail.com>
2022-11-05 11:41:53 +0530

    on clicking categorieItems in category componenet you will be redirected to productList.jsx page

--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import styled from "styled-components"
 import { mobile } from "../responsive"
 
@@ -41,12 +42,14 @@ font-weight: 600;
 const CategoryItem = ({ item }) => {
     return (
         <Container>
+            <Link to={`/products/${item.cat}`}>
             <Image src={item.img} />
             <Info>
                 <Title>{item.title}</Title>
                 <Button>SHOP NOW</Button>
             </Info>
-        </Container>
+        </Link>
+        </Container >
 
     )
 }
